5b1b506fa539fdd998fec784af4fd7d6b475c19c
Bipul-Ch-Roy <kumarroy.kb.kb@gmail.com>
2021-09-16 03:18:05 +0600

    artcell song added

--- script.js
+++ script.js
@@ -2,8 +2,23 @@ let music = document.querySelector('audio');
 let toggleBtn = document.getElementById('toggleBtn');
 let isPlaying = false;
 let audioPlayer = document.getElementById('audioPlayer');
+let audioImage = document.getElementById('Image');
+let songName = document.getElementById('songName');
 let songIndex = 0;
-const musicList = ['aud/Alo-Alo_Tahsan.mp3','aud/ek-paye-nupur.mp3','aud/Epitaph-Shonar-Bangla-Circus.mp3'];
+let musicList = ['aud/Alo-Alo_Tahsan.mp3','aud/ek-paye-nupur.mp3','aud/Dhusor somoy.mp3','aud/Dukkho_Bilash.mp3','aud/Epitaph.mp3'];
+const imgList = ['img/alo alo.jpg','img/ek paye.jpg','img/artcell.jpg','img/artcell.jpg','img/sonar bangla.jpg'];
+
+
+const metal = () => {
+    // musicList = ['aud/Dhusor somoy.mp3','aud/Dukkho_Bilash.mp3'];
+    alert('This feature will come soon.Plese wait few days.thanked by bipul chandro roy')
+}
+const soft = () => {
+    // musicList = ['aud/Dhusor somoy.mp3','aud/Dukkho_Bilash.mp3'];
+    alert('Already selected')
+}
+// let newList = metal();
+// musicList = newList;
 
 
 //////////////////////////// music play & pause function starts here //////////////////////////////////////////
@@ -18,8 +33,9 @@ const playAudio = () => {
 const playMusic = () => {
     isPlaying = true;
     music.play();
-    toggleBtn.classList.add('fa-pause-circle');
-    toggleBtn.classList.remove('fa-play');
+    toggleBtn.classList.add('fa-pause-circle','text-primary');
+    toggleBtn.classList.remove('fa-play','text-success');
+    
 }
 //////////////////////////////////////////// play button ends here /////////////////////////////////////////////
 
@@ -29,26 +45,37 @@ const playMusic = () => {
 const pauseMusic = () => {
     isPlaying = false;
     music.pause();
-    toggleBtn.classList.add('fa-play');
-    toggleBtn.classList.remove('fa-pause-circle');
+    toggleBtn.classList.add('fa-play','text-success');
+    toggleBtn.classList.remove('fa-pause-circle','text-primary');
 }
 //////////////////////////////////////////// play button ends here /////////////////////////////////////////////
 
 
+
+//////////////////////////////////////////// next button starts here /////////////////////////////////////////////
 const nextBtn = () => {
     songIndex++
     if (songIndex >= musicList.length) {
         songIndex = 0;
     }
     audioPlayer.setAttribute('src',musicList[songIndex]);
+    audioImage.setAttribute('src',imgList[songIndex]);
+    songName.innerText = musicList[songIndex]
     music.play();
 }
+//////////////////////////////////////////// next button ends here /////////////////////////////////////////////
 
+
+
+//////////////////////////////////////////// prev button starts here /////////////////////////////////////////////
 const preBtn = () => {
     songIndex--
     if (songIndex <= -1) {
         songIndex = musicList.length - 1;
     }
     audioPlayer.setAttribute('src',musicList[songIndex]);
+    audioImage.setAttribute('src',imgList[songIndex]);
+    songName.innerText = musicList[songIndex]
     music.play();
 }
+//////////////////////////////////////////// prev button ends here /////////////////////////////////////////////
